1899e01acc8e1da0069eb5ae7777ea4e8d236d35
rajdeeprockysarker <rajdeepsarker1986@gmail.com>
2022-02-09 13:14:36 +0530

    Ui Change

--- SIPPUSH/app/src/main/java/com/sip/sippush/ContactActivity.java
+++ SIPPUSH/app/src/main/java/com/sip/sippush/ContactActivity.java
@@ -29,6 +29,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -62,6 +63,12 @@ import java.util.ArrayList;
 
 public class ContactActivity extends AppCompatActivity {
 
+    EditText edt_firstname, edt_surname, edt_mobile_number, edt_home_number, edt_work_number,
+            edt_sipaddress, edt_home_email, edt_work_email, edt_address, edt_website, edt_organization_name, edt_organization_job_title,edt_ProfilePic;
+
+    Button btn_check_url;
+    ImageView img_image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +77,41 @@ public class ContactActivity extends AppCompatActivity {
         contactPermission();
 
         Button btn_contact_save = (Button) findViewById(R.id.btn_contact_save);
+        btn_check_url = (Button) findViewById(R.id.btn_check_url);
+        img_image = (ImageView) findViewById(R.id.img_image);
+
+
+        edt_firstname = (EditText) findViewById(R.id.edt_firstname);
+        edt_surname = (EditText) findViewById(R.id.edt_surname);
+        edt_mobile_number = (EditText) findViewById(R.id.edt_mobile_number);
+        edt_home_number = (EditText) findViewById(R.id.edt_home_number);
+        edt_work_number = (EditText) findViewById(R.id.edt_work_number);
+        edt_sipaddress = (EditText) findViewById(R.id.edt_sipaddress);
+        edt_home_email = (EditText) findViewById(R.id.edt_home_email);
+        edt_work_email = (EditText) findViewById(R.id.edt_work_email);
+        edt_address = (EditText) findViewById(R.id.edt_address);
+        edt_website = (EditText) findViewById(R.id.edt_website);
+        edt_organization_name = (EditText) findViewById(R.id.edt_organization_name);
+        edt_organization_job_title = (EditText) findViewById(R.id.edt_organization_job_title);
+        edt_ProfilePic = (EditText) findViewById(R.id.edt_ProfilePic);
+
+        btn_check_url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Glide.with(ContactActivity.this).asBitmap().load(edt_ProfilePic.getText().toString()).into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        img_image.setImageBitmap(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
+            }
+        });
+
 
         btn_contact_save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,7 +123,8 @@ public class ContactActivity extends AppCompatActivity {
 
     }
 
-    private void addContact(String phoneticname,String phoneticSurname,String phoneticMiddle,String given_name, String name, String mobile, String mobileHome, String mobileWork, String homenumber, String worknumber, String emailhome, String workhome, String sipaddress) {
+    private void addContact(String phoneticname, String phoneticSurname, String phoneticMiddle, String given_name, String name, String mobile, String homenumber, String worknumber, String emailhome, String workhome, String sipaddress
+            , String address, String website, String organization_name, String organization_job_title) {
         ArrayList<ContentProviderOperation> contact = new ArrayList<ContentProviderOperation>();
 
         contact.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
@@ -134,12 +177,12 @@ public class ContactActivity extends AppCompatActivity {
                 .build());
 
         // Contact No Mobile home
-        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileHome)
-                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
-                .build());
+//        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+//                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+//                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+//                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileHome)
+//                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
+//                .build());
 
         // Contact Home
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
@@ -161,7 +204,7 @@ public class ContactActivity extends AppCompatActivity {
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Email.DATA, emailhome)
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, workhome)
                 .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
                 .build());
 
@@ -169,7 +212,7 @@ public class ContactActivity extends AppCompatActivity {
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Email.DATA, workhome)
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, emailhome)
                 .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_HOME)
                 .build());
 
@@ -181,13 +224,12 @@ public class ContactActivity extends AppCompatActivity {
                 .withValue(ContactsContract.CommonDataKinds.SipAddress.TYPE, ContactsContract.CommonDataKinds.SipAddress.TYPE_WORK)
                 .build());
 
-        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.SipAddress.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.SipAddress.DATA, sipaddress)
-                .withValue(ContactsContract.CommonDataKinds.SipAddress.TYPE, ContactsContract.CommonDataKinds.SipAddress.TYPE_HOME)
-                .build());
-
+//        contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+//                .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+//                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.SipAddress.CONTENT_ITEM_TYPE)
+//                .withValue(ContactsContract.CommonDataKinds.SipAddress.DATA, sipaddress)
+//                .withValue(ContactsContract.CommonDataKinds.SipAddress.TYPE, ContactsContract.CommonDataKinds.SipAddress.TYPE_HOME)
+//                .build());
 
 
         /// Organization
@@ -196,20 +238,18 @@ public class ContactActivity extends AppCompatActivity {
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
                 .withValue(ContactsContract.Data.MIMETYPE,
                         ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, "company")
+                .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, organization_name)
                 .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
-                .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, "jobTitle")
+                .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, organization_job_title)
                 .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
                 .build());
 
 
-
-
-      /// Website Address
+        /// Website Address
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Website.URL, "www.Address.com")
+                .withValue(ContactsContract.CommonDataKinds.Website.URL, website)
                 .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
                 .build());
 
@@ -218,20 +258,17 @@ public class ContactActivity extends AppCompatActivity {
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS, "Circular Road, Mumbai")
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS, address)
                 .withValue(ContactsContract.CommonDataKinds.StructuredPostal.TYPE, ContactsContract.CommonDataKinds.StructuredPostal.TYPE_WORK)
                 .build());
-
-
-
 
 
         // add the photo
         contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo. CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE)
                 .withValue(ContactsContract.CommonDataKinds.Photo.PHOTO, bitmapByteArray)
-                        .build());
+                .build());
 
         try {
             ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, contact);
@@ -240,23 +277,55 @@ public class ContactActivity extends AppCompatActivity {
         }
     }
 
-    byte[] bitmapByteArray=null;
+    byte[] bitmapByteArray = null;
 
-    public void downloadimage(){
-        Glide.with(this).asBitmap().load("https://mma.prnewswire.com/media/197347/kroger_co_logo.jpg").into(new CustomTarget<Bitmap>() {
+    public void downloadimage() {
+        Glide.with(this).asBitmap().load(edt_ProfilePic.getText().toString()).into(new CustomTarget<Bitmap>() {
             @Override
             public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
                 bitmapByteArray = toByteArray(resource);
-                addContact( "Phoneticname","Phoneticname Surname","Phoneticname Middle","123456","123456Givenname","123456789","1111111111","2222222222","3333333333",
-                        "4444444444","home@email.com","work@email.com","sip@123@gmail.com");
+                addContact("Phoneticname", "Phoneticname Surname", "Phoneticname Middle",
+                        edt_firstname.getText().toString(),
+                        edt_surname.getText().toString(),
+                        edt_mobile_number.getText().toString(),
+                        edt_home_number.getText().toString(),
+                        edt_work_number.getText().toString(),
+                        edt_home_email.getText().toString(),
+                        edt_work_email.getText().toString(),
+                        edt_sipaddress.getText().toString(),
+                        edt_address.getText().toString(),
+                        edt_website.getText().toString(),
+                        edt_organization_name.getText().toString(),
+                        edt_organization_job_title.getText().toString()
+                );
 
             }
+
             @Override
             public void onLoadCleared(@Nullable Drawable placeholder) {
-                bitmapByteArray=null;
-                addContact( "Phoneticname","Phoneticname Surname","Phoneticname Middle","123456","123456Givenname","123456789","1111111111","2222222222","3333333333",
-                        "4444444444","home@email.com","work@email.com","sip@123@gmail.com");
+                bitmapByteArray = null;
 
+
+                edt_address = (EditText) findViewById(R.id.edt_address);
+                edt_website = (EditText) findViewById(R.id.edt_website);
+                edt_organization_name = (EditText) findViewById(R.id.edt_organization_name);
+                edt_organization_job_title = (EditText) findViewById(R.id.edt_organization_job_title);
+
+
+                addContact("Phoneticname", "Phoneticname Surname", "Phoneticname Middle",
+                        edt_firstname.getText().toString(),
+                        edt_surname.getText().toString(),
+                        edt_mobile_number.getText().toString(),
+                        edt_home_number.getText().toString(),
+                        edt_work_number.getText().toString(),
+                        edt_home_email.getText().toString(),
+                        edt_work_email.getText().toString(),
+                        edt_sipaddress.getText().toString(),
+                        edt_address.getText().toString(),
+                        edt_website.getText().toString(),
+                        edt_organization_name.getText().toString(),
+                        edt_organization_job_title.getText().toString()
+                );
             }
         });
     }
@@ -275,10 +344,10 @@ public class ContactActivity extends AppCompatActivity {
             if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(ContactActivity.this, new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 123);
             } else {
-               // permissionOperation = true;
+                // permissionOperation = true;
             }
         } else {
-          //  permissionOperation = true;
+            //  permissionOperation = true;
         }
     }
 
---
+++ SIPPUSH/app/src/main/java/com/sip/sippush/LandingActivity.java
@@ -0,0 +1,56 @@
+package com.sip.sippush;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.net.sip.SipException;
+import android.net.sip.SipManager;
+import android.net.sip.SipProfile;
+import android.net.sip.SipRegistrationListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.downloader.Error;
+import com.downloader.OnCancelListener;
+import com.downloader.OnDownloadListener;
+import com.downloader.OnPauseListener;
+import com.downloader.OnProgressListener;
+import com.downloader.OnStartOrResumeListener;
+import com.downloader.PRDownloader;
+import com.downloader.PRDownloaderConfig;
+import com.downloader.Progress;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class LandingActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
